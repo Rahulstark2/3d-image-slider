d9b271146f359dd3b7ef853771a7945109cab81d
Rahulstark2 <rahulstark2@gmail.com>
2024-10-10 19:29:42 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,43 @@
+export const slides = [
+    {
+      title: "Luminary",
+      id: "CG30128-LUM",
+    },
+    {
+      title: "Spark",
+      id: "CG39102-SPR",
+    },
+    {
+      title: "Resonance",
+      id: "CG84729-RES",
+    },
+    {
+      title: "Fracture",
+      id: "CG67215-FRA",
+    },
+    {
+      title: "Beat",
+      id: "CG48391-BEA",
+    },
+    {
+      title: "Enigma",
+      id: "CG15938-ENI",
+    },
+    {
+      title: "Stellar",
+      id: "CG92457-STE",
+    },
+    {
+      title: "Harmony",
+      id: "CG54283-HAR",
+    },
+    {
+      title: "Flow",
+      id: "CG61872-FLO",
+    },
+    {
+      title: "Mist",
+      id: "CG73190-MIS",
+    },
+  ];
+  
